81f7eedd95238b059ae55c4d11afc9c8a6b6cd5b
youssef <benotsman.youssuf@gmail.com>
2024-04-09 23:51:20 +0100

    adding some hadith books

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const btnElement = document.getElementById('btn');
 const numberElement = document.getElementById('number');
 const sourceElement = document.getElementById('source');
 
-let schollars = ['bukhari', 'muslim'];
+let schollars = ['bukhari', 'muslim', "abu-daud", "ahmad", "darimi", "ibnu-majah", "malik", "nasai", "tirmidzi" ]
 
 
 const fetchHadith = async () => {
     try {
-    const response = await fetch('https://api.hadith.gading.dev/books/'+schollars[getRandomNumberFrom1To2()-1]+'/'+getRandomNumber());
+    const response = await fetch('https://api.hadith.gading.dev/books/'+schollars[getRandomNumberFrom0To9()]+'/'+getRandomNumber());
     const data = await response.json();
 
     const arabicText = data.data.contents.arab;
@@ -26,6 +26,27 @@ const fetchHadith = async () => {
     else if (source === 'muslim') {
         sourceElement.textContent = 'صحيح مسلم';
     }
+    else if (source === 'abu-daud') {
+        sourceElement.textContent = 'سنن أبي داود';
+    }
+    else if (source === 'ahmad') {
+        sourceElement.textContent = 'مسند أحمد';
+    }
+    else if (source === 'darimi') {
+        sourceElement.textContent = 'سنن الدارمي';
+    }
+    else if (source === 'ibnu-majah') {
+        sourceElement.textContent = 'سنن ابن ماجه';
+    }
+    else if (source === 'malik') {
+        sourceElement.textContent = 'موطأ مالك';
+    }
+    else if (source === 'nasai') {
+        sourceElement.textContent = 'سنن النسائي';
+    }
+    else if (source === 'tirmidzi') {
+        sourceElement.textContent = 'سنن الترمذي';
+    }
     } catch (error) {
     console.error('Error fetching hadith:', error);
     }
@@ -39,8 +60,6 @@ btnElement.addEventListener('click', fetchHadith);
 
 // for random numvers
 const getRandomNumber = () => {
-    return Math.floor(Math.random() * 4000) + 1;
+    return Math.floor(Math.random() * 2000) + 1;
 };
-const getRandomNumberFrom1To2 = () => {
-    return Math.floor(Math.random() * 2) + 1;
-};
+const getRandomNumberFrom0To9 = () => { return Math.floor(Math.random() * 10); };
